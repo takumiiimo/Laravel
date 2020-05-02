10655ac12cfe479c0e564aad112406fa8b655af1
takumiiimo <assistant2370@gmail.com>
2020-05-02 07:35:35 +0000

    PHP/Laravel 10

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function(){
     Route::get('news/create', 'Admin\NewsController@add');
     Route::get('XXX','Admin\AAAController@bbb');
-    Route::get('profile/cteate', 'Admin\ProfileController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     
 });
