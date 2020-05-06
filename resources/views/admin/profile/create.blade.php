5707ebef0d204ab4e76c639acf06ba7acf82ca85
takumiiimo <assistant2370@gmail.com>
2020-05-06 08:12:07 +0000

    PHP/Laravel 12

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,11 +1,11 @@
-{{-- layouts/admin.blade.phpを読み込む --}}
+
 @extends('layouts.profile')
 
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+
 @section('title', 'プロフィールの新規作成')
 
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+
 @section('content')
     <div class="container">
         <div class="row">
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,23 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('XXX','Admin\AAAController@bbb');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
-    
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+    
+    
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
